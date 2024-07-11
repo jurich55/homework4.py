14fc221284ecf5f5a8249acc1daeb720f1571e12
Vladimir <sv_urich@mail.ru>
2024-07-11 19:53:47 +0400

    Add files via upload

---
+++ homework4.py/main.py
@@ -0,0 +1,8 @@
+my_string = input("Напишите какой сегодня день недели и число: ")
+# понедельник 15 июля
+print(len(my_string))
+print(my_string.upper())
+print(my_string.upper().lower())
+print(my_string.replace(' ',''))
+print((my_string)[0])
+print((my_string)[-1:])
